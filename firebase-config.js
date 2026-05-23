95a55fe4b5d42d2571c2ade86db4107937a98d42
DEUZIMAR CAMPOS <135393188+PROMAZIR@users.noreply.github.com>
2026-05-23 11:03:23 -0300

    alteração da chave

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyBdX2N-8POz7hfEcfKhgU-N94MrWf8Qp0Q",
+const firebaseConfig = {
+  apiKey: "AIzaSyCuWRiH0xifQ48yRL_0Tv-vMwJdo3Az1Y0",
   authDomain: "rifa-luiz-3ano.firebaseapp.com",
   projectId: "rifa-luiz-3ano",
   storageBucket: "rifa-luiz-3ano.firebasestorage.app",
   messagingSenderId: "331354764919",
-  appId: "1:331354764919:web:03f101995f930b70537872",
-  measurementId: "G-6L7HXSSN93",
+  appId: "1:331354764919:web:e30836c88636b8c5537872",
+  measurementId: "G-7F3NQB17R9"
 };
